f71862eac7dc5deda645312fdf44259b94285f86
xiaobaibai <xiaobaibai@MacBook-Air-2.local>
2024-05-22 14:03:48 +0800

    Initial commit

--- components/VideoPlayer.tsx
+++ components/VideoPlayer.tsx
@@ -1,7 +1,7 @@
 'use client';
 
-import '@vidstack/react/player/styles/base.css';
-import '@vidstack/react/player/styles/plyr/theme.css';
+import '@vidstack/react/player/styles/default/theme.css';
+import '@vidstack/react/player/styles/default/layouts/video.css';
 import {
   MediaPlayer,
   MediaProvider,
@@ -10,7 +10,7 @@ import {
   type MediaProviderChangeEvent,
 } from '@vidstack/react';
 import { Gesture } from '@vidstack/react';
-import { PlyrLayout, plyrLayoutIcons } from '@vidstack/react/player/layouts/plyr';
+import { defaultLayoutIcons, DefaultVideoLayout } from '@vidstack/react/player/layouts/default';
 import HLS from 'hls.js';
 
 type VideoPlayerProps = {
@@ -47,7 +47,7 @@ export default function VideoPlayer({ title,src}: VideoPlayerProps) {
       <Gesture className="vds-gesture" event="dblpointerup" action="seek:-10" />
       <Gesture className="vds-gesture" event="dblpointerup" action="seek:10" />
       <Gesture className="vds-gesture" event="dblpointerup" action="toggle:fullscreen" />
-      <PlyrLayout icons={plyrLayoutIcons} />
+      <DefaultVideoLayout icons={defaultLayoutIcons} />
     </MediaPlayer>
   );
 }
